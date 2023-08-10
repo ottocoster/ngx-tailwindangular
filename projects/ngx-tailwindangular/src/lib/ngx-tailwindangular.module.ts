d8145f9c979b641adf5fd5a8df1e961dab631084
Otto Coster <otto@ottocoster.com>
2023-08-10 14:29:19 +0200

    Added DropdownMenu directive

---
+++ projects/ngx-tailwindangular/src/lib/ngx-tailwindangular.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { DropdownMenuDirective } from './dropdown-menu.directive';
+
+@NgModule({
+  declarations: [DropdownMenuDirective],
+  imports: [],
+  exports: [DropdownMenuDirective],
+})
+export class NgxTailwindangularModule {}
---
+++ projects/ngx-tailwindangular/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ngx-tailwindangular
+ */
+
+export * from './lib/ngx-tailwindangular.service';
+export * from './lib/ngx-tailwindangular.component';
+export * from './lib/ngx-tailwindangular.module';
