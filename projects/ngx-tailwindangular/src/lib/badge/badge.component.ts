5864548fed11d1d1832dda3426f7498e0f9a9a7e
Otto Coster <otto@ottocoster.com>
2024-04-29 16:15:52 +0200

    Add badge

---
+++ projects/ngx-tailwindangular/src/lib/badge/badge.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ta-badge',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './badge.component.html',
+})
+export class BadgeComponent {}
